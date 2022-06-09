dac0ab0064fc8c26e6052a3bf4da81255807853e
Erica Fischer <enf@pobox.com>
2022-06-08 21:08:57 -0700

    Go back to the roads shapefile

--- blocks.py
+++ blocks.py
@@ -1,59 +1,77 @@
 #!/usr/bin/python
 
 import json
+import math
 
-with open('tl_2021_06001_edges.json') as f:
+with open('tl_2021_06001_roads.json') as f:
 	features = json.load(f)
 
-street_blocks_from = {}
-street_blocks_to = {}
+point_instances = {}
+
+def dist(a, b):
+	dx = a[0] - b[0]
+	dy = a[1] - b[1]
+	scale = math.cos(a[1] * math.pi / 180.0)
+	dx = dx * scale
+	return math.sqrt(dx * dx + dy * dy) / .00000274 / 5280
 
 for feature in features['features']:
-	# skip non-roads
-	if feature['properties']['MTFCC'][0] != 'S':
-		continue
-
-	# skip unnamed roads
-	name = feature['properties']['FULLNAME']
-	if name == '':
-		continue
-
 	# make sure assumption of LineString geometry is correct
 	if feature['geometry']['type'] != 'LineString':
 		die(feature['geometry']['type'])
 
-	if name not in street_blocks_from:
-		street_blocks_from[name] = {}
-		street_blocks_to[name] = {}
-		# identify *some* feature in each named street
+	# count the number of features that each point appears in,
+	# because the points that appear in multiple features
+	# are the intersections
+	for point in feature['geometry']['coordinates']:
+		p = str(point)
+		if p in point_instances:
+			point_instances[p] = point_instances[p] + 1
+		else:
+			point_instances[p] = 1
 
-	street_blocks_from[name][feature['properties']['TNIDF']] = feature
-	street_blocks_to[name][feature['properties']['TNIDT']] = feature
+for feature in features['features']:
+	geom = feature['geometry']['coordinates']
 
-for street in street_blocks_from:
-	froms = street_blocks_from[street]
-	tos = street_blocks_to[street]
+	i = 0
+	while i < len(geom):
+		total = 0
 
-	print(street)
-	# there may be multiple streets with the same name, so
-	# make sure we do them all
-	while len(froms) > 0:
-		block = list(froms)[0]
-		print(block)
+		# walk forward until there is at least a quarter mile
+		for j in range(i + 1, len(geom)):
+			total = total + dist(geom[j - 1], geom[j])
+			if total >= 0.25:
+				break
 
-		# back off to the first block of this street
-		while block in tos and tos[block]['properties']['TNIDF'] in froms and tos[block]['properties']['TNIDF'] != block:
-			block = tos[block]['properties']['TNIDF']
-			print("back off", block)
+		# continue walking forward while not at an intersection
+		while j < len(geom) and point_instances[str(geom[j])] == 1:
+			total = total + dist(geom[j - 1], geom[j])
+			j = j + 1
 
-		print(street, block)
-		print(list(froms))
+		# skip streets that cannot be extended 0.25 miles from their origin
+		if (i == 0 or j == len(geom)) and total < 0.25:
+			break
 
-		feat = froms[block]
+		obj = {
+			'type': 'Feature',
+			'properties': {
+				'name': feature['properties']['FULLNAME'],
+				'start': i,
+				'end': j,
+				'distance': total
+			},
+			'geometry': {
+				'type': 'LineString',
+				'coordinates': []
+			}
+		}
 
-		# remove the block from the available list,
-		# in both directions
+		for k in range(i, j):
+			obj['geometry']['coordinates'].append(geom[k])
 
-		del froms[block]
-		if feat['properties']['TNIDT'] in tos:
-			del tos[feat['properties']['TNIDT']]
+		print(json.dumps(obj))
+
+		# walk i forward to the next intersection too:
+		i = i + 1
+		while i < len(geom) and point_instances[str(geom[i])] == 1:
+			i = i + 1
